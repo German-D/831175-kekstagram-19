4da3bf395c9b5dc7adc9453e40a325a32c2be34e
German <dolnikov@edadeal.ru>
2020-01-29 22:17:36 +0300

    Правки по ревью

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@
 
 var i;
 
-var photos = [];
-var comments = [];
-
 var names = [
   'German',
   'Ann',
@@ -51,20 +48,35 @@ var messages = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-// Функция поиска случайного чилса. Максимум и минимум включаются
+// Функция для генерации массива вида 1,2…100
+function getArray(a, b) {
+  if (a >= b) {
+    return [];
+  }
+  var r = [];
+  for (var n = a; n <= b; n++) {
+    r.push(i);
+  }
+  return r;
+}
+
+// Функция получения случайного целого числа в заданном интервале. Максимум и минимум включаются
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var random25; // Случайное число от 1 до 25 включительно
-var random6; // Случайное число от 1 до 6 включительно
-var random2; // Случайное число от 1 до 2 включительно
-var newMessage;
+// Функция поиска случайного элемента в массиве
+function getRandomElement(array) {
+  var getRandomElementOrder = getRandomIntInclusive(0, array.length);
+  return array[getRandomElementOrder];
+}
 
 // Функция для создания описания
 function addNewMessage(messageList) {
+  var random2; // Случайное число от 1 до 2 включительно
+  var newMessage;
   random2 = getRandomIntInclusive(1, 2);
   if (random2 === 1) {
     newMessage = messageList[getRandomIntInclusive(0, 6)];
@@ -75,42 +87,45 @@ function addNewMessage(messageList) {
 }
 
 // Функция для создания комментария
-function addNewComment(messagesList, namesList) {
-  for (i = 0; i < getRandomIntInclusive(1, 10); i++) {
+function addNewComment(messagesArray, namesArray) {
+  var comments = [];
+  var random10 = getRandomIntInclusive(1, 10); // Случайное число от 1 до 10 включительно
+  var random6; // Случайное число от 1 до 6 включительно
+  for (i = 0; i < random10; i++) {
     random6 = getRandomIntInclusive(1, 6);
     comments.push({
       avatar: 'img/avatar-' + random6 + '.svg',
-      message: addNewMessage(messagesList),
-      name: namesList[getRandomIntInclusive(0, 24)]
+      message: addNewMessage(messagesArray),
+      name: namesArray[getRandomIntInclusive(0, 24)]
     });
   }
   return comments;
 }
 
 // Функция для создания новой фотографии
-function addNewPhoto(namesList, messagesList, descriptionsList) {
+function addNewPhoto(namesArray, messagesArray, descriptionsArray) {
+  var photos = [];
+  var random25; // Случайное число от 1 до 25 включительно
+  var newArray = getArray(1, 25); // Создаю новый массив от 1 до 25
 
-  var imgCache = {}; // Переменная для уникальных значений картинок
+  for (var k = 1; k < 25; k++) {
+    random25 = getRandomElement(newArray); // Присваиваю random25 случайное значение из массива
 
-  while (photos.length <= 24) {
-    random25 = getRandomIntInclusive(1, 25);
+    var random25Position = newArray.indexOf(random25); // Вычиляю позицию random25 в массиве newArray
+    newArray.splice(random25Position, 1); // Удаляю позицию random25 в массиве newArray
 
-    while (imgCache[random25]) {
-      random25 = getRandomIntInclusive(1, 25);
-    }
-
-    imgCache[random25] = true;
     photos.push({
       url: 'photos/' + random25 + '.jpg',
-      description: descriptionsList[getRandomIntInclusive(0, 6)],
+      description: descriptionsArray[getRandomIntInclusive(0, 6)],
       likes: getRandomIntInclusive(15, 200),
-      comments: addNewComment(messagesList, namesList)
+      comments: addNewComment(messages, names)
     });
   }
   return photos;
 }
 
-addNewPhoto(names, messages, descriptions);
+var photosArray = addNewPhoto(names, messages, descriptions);
+
 
 var photoTemplate = document.querySelector('#picture');
 var photoList = document.querySelector('.pictures');
@@ -126,8 +141,8 @@ function renderPhoto(photosList) {
 }
 
 var fragment = document.createDocumentFragment();
-for (i = 0; i < photos.length; i++) {
-  fragment.appendChild(renderPhoto(photos[i]));
+for (i = 0; i < photosArray.length; i++) {
+  fragment.appendChild(renderPhoto(photosArray[i]));
 }
 
 photoList.appendChild(fragment);
